0b98fec675b2185919218ec472f6e98b121948de
Sco0b1 <zakhar.vasiukov@gmail.com>
2023-01-10 06:18:02 +0100

    Cesar cipher

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#define shift 3
+using namespace std;
+
+string encrypt(const string& text) {
+    string result;
+    for (char c : text) {
+        if (isalpha(c)) {
+            char offset = isupper(c) ? 'A' : 'a';
+            result += char(((c - offset + shift) % 26) + offset);
+        } else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main() {
+    string text;
+    cout << "Enter text to encrypt: \n";
+    getline(std::cin, text);
+
+
+    string encrypted = encrypt(text);
+    cout << "Encrypted text: \n" << encrypted << endl;
+    return 0;
+}
